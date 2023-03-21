892f16099bad621de54ba14aedc5df2904b1494f
El Jefe <Tagne13@gmail.com>
2023-03-20 20:41:18 -0400

    added API info to js and added clear button to html

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,23 @@
-var APIKey = "55c7242f8bf237f063bd07fa75719af2";
+$(document).ready(function () {
+    const searchBtn = $("#searchBtn");
+    const clearBtn = $("#clearBtn");
+    let searchCity = $("#city");
+
+
+
+    function currentQueryURL() {
+    // Weather API URL
+    var currentURL = `https://api.openweathermap.org/data/2.5/weather?`;
+
+    // API Key
+    var APIKey = { "APPID": "55c7242f8bf237f063bd07fa75719af2" };
+
+    // Search term
+    APIKey.q = searchCity
+        .val()
+        .trim();
+
+    // Generate URL
+    return currentURL + $.param(APIKey);
+    }
+});
